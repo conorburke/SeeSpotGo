35e82a3b879848bc06b7e09915d75024e20792f9
conorburke <cjburke89@gmail.com>
2016-12-18 14:45:55 -0800

    pass association and basic validation tests

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,2 +1,14 @@
 class Reservation < ApplicationRecord
+  belongs_to :space
+  belongs_to :occupant, class_name: :User
+  has_many :ratings
+
+  validates :space_id, presence: true
+  validates :occupant_id, presence: true
+
+  validate :start_time_is_valid_datetime
+
+  def start_time_is_valid_datetime
+    errors.add(:happened_at, 'must be a valid datetime') if ((DateTime.parse(happened_at) rescue ArgumentError) == ArgumentError)
+  end
 end
--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Reservation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let (:occupant) {User.create(first_name: "Conor", last_name: "Burke", email: "conor@gmail.com", password_digest: "1234", phone: "123-456-7890")}
+  let (:location) {Location.create(user_id: occupant.id, street_address: "707 Broadway Avenue", city: "San Diego", state: "CA", zip: "92101")}
+  let (:space) {Space.create(location_id: location.id)}
+  let (:reservation) {Reservation.create(space_id: space.id, occupant_id: occupant.id)}
+  # pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe 'associations' do 
+    it { should belong_to(:occupant) }
+    it { should belong_to(:space) }
+  end
+
+  describe 'validations' do 
+    it { is_expected.to validate_presence_of(:occupant_id) }
+    it { is_expected.to validate_presence_of(:space_id) }
+  end
 end
