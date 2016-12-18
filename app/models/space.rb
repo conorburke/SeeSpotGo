3ce5ecd677019528cfd264f73df7956dc6ff7f92
Kelsey Edelstein <email": null>
2016-12-18 15:06:16 -0800

    Update space model association

--- app/models/space.rb
+++ app/models/space.rb
@@ -1,6 +1,6 @@
 class Space < ApplicationRecord
   belongs_to :location
-  has_one :owner, through: :location
+  has_one :owner, through: :location, foreign_key: :user_id
 
   validates_presence_of :location, :owner, :description
   validates :size, inclusion: { in: %w(motorcycle compact standard large RV),
