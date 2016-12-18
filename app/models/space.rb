1584607ab194da6f6ebe157aa059773eab8f72b2
Patrick Anderson <email": null>
2016-12-18 14:54:53 -0800

    validations

--- app/models/space.rb
+++ app/models/space.rb
@@ -2,10 +2,8 @@ class Space < ApplicationRecord
   belongs_to :location
   has_one :owner, through: :location
 
-  validates :location, presence: true
-  validates :owner, presence: true
+  validates_presence_of :location, :owner, :description
   validates :size, inclusion: { in: %w(motorcycle compact standard large RV),
     message: "%{value} is not a valid size" }
-  validates :description, presence: true
   validates :space_active, inclusion: { in: [0,1] }
 end
