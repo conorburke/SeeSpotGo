f038155130cc20145cfa59735403d55c85d4a5e0
Patrick Anderson <email": null>
2016-12-18 14:44:10 -0800

    space model finished?

--- app/models/space.rb
+++ app/models/space.rb
@@ -4,6 +4,8 @@ class Space < ApplicationRecord
 
   validates :location, presence: true
   validates :owner, presence: true
-
-
+  validates :size, inclusion: { in: %w(motorcycle compact standard large RV),
+    message: "%{value} is not a valid size" }
+  validates :description, presence: true
+  validates :space_active, inclusion: { in: [0,1] }
 end
