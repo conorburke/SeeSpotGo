ad2e72cf439fbf5c87644b43ea0717f9331c1160
Kelsey Edelstein <email": null>
2016-12-19 15:59:07 -0800

    Fix reservation bug

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,7 +1,7 @@
 class ReservationsController < ApplicationController
   def create
-    @reservation = Reservation.create(start_time: params[:start_time], end_time: params[:end_time])
-    @space = Space.find(@reservation.space_id)
-    render space_path(@space)
+    @space = Space.find(params[:space_id])
+    @space.reservations.create(start_time: params[:start_time], end_time: params[:end_time])
+    redirect_to space_path(@space)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { registrations: 'users/registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :spaces
-  post 'reservations' => 'reservations#create'
+  resources :spaces do
+    resources 'reservations', only: [:create]
+  end
+  # post 'reservations' => 'reservations#create'
   root to: "home#index"
 end
