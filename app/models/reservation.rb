a4c43aa28196d4191784824ead9f608347213109
conorburke <cjburke89@gmail.com>
2016-12-18 16:16:42 -0800

    create and pass reservation model tests

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -5,10 +5,28 @@ class Reservation < ApplicationRecord
 
   validates :space_id, presence: true
   validates :occupant_id, presence: true
+  validates :start_time, presence: true
+  validates :end_time, presence: true
 
-  validate :start_time_is_valid_datetime
+  validate :ensure_start_time_is_valid_datetime
+  validate :ensure_end_time_is_valid_datetime
 
-  def start_time_is_valid_datetime
-    errors.add(:happened_at, 'must be a valid datetime') if ((DateTime.parse(happened_at) rescue ArgumentError) == ArgumentError)
+  validate :start_time_is_in_future, on: :create
+  validate :end_time_is_in_future, on: :create
+
+  def ensure_start_time_is_valid_datetime
+    errors.add(:start_time, 'must be a valid datetime') unless (self.start_time.is_a?(ActiveSupport::TimeWithZone))
+  end
+
+  def ensure_end_time_is_valid_datetime
+    errors.add(:end_time, 'must be a valid datetime') unless (self.end_time.is_a?(ActiveSupport::TimeWithZone))
+  end
+
+  def start_time_is_in_future
+    errors.add(:start_time, 'must be in the future') if (self.start_time && self.start_time.past?)
+  end
+
+  def end_time_is_in_future
+    errors.add(:end_time, 'must be in the future') if (self.start_time && self.end_time.past?)
   end
 end
