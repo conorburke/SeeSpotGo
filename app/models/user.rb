9d02c25531622a6d6e772166be7a3516ae96153b
Qiaoshu Lin <email": null>
2016-12-18 16:14:53 -0800

    Add and test more associations to user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,25 +2,25 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :locations
-  has_many :reservations, foreign_key: :occupant_id
+  has_many :spaces, through: :locations
+  has_many :received_reservations, through: :spaces, source: :reservations
+  has_many :requested_reservations, class_name: :Reservation, foreign_key: :occupant_id
+  has_many :written_ratings, class_name: :Rating, foreign_key: :rater_id
+  has_many :received_ratings, class_name: :Rating, foreign_key: :user_id
 
-  def ratings
-    Rating.where("user_id = ? OR rater_id = ?", self.id, self.id)
-  end
-
-  def average_rating
-    ratings = self.ratings.pluck(:score)
-    ratings.reduce(:+) ? ratings.reduce(:+)/ratings.size : 0
-  end
-
-  validates :first_name, :last_name, :email, :password_digest, :phone, :status, presence: true
+  validates :first_name, :last_name, :email, :password_digest, :phone, presence: true
   validates :email, uniqueness: { case_sensitive: false }
 
   validate :valid_phone
 
   def valid_phone
     unless self.phone =~ /^\d{3}-\d{3}-\d{4}$/ # Ex: "619-643-8612"
-      errors.add :phone, "number must be valid"
+      errors.add :phone, "number must be valid format"
     end
+  end
+
+  def average_rating
+    ratings = self.received_ratings.pluck(:score)
+    ratings.reduce(:+) ? (ratings.reduce(:+)/ratings.size.to_f).round(1) : 0
   end
 end
