c875d34b565cc180e505d9d05d2c1885639e174d
Kelsey Edelstein <email": null>
2016-12-20 11:01:33 -0800

    Save ratings to database, show average rating

---
+++ app/controllers/ratings_controller.rb
@@ -0,0 +1,11 @@
+class RatingsController < ApplicationController
+  def create
+    puts "askdfjhaskdfjhaskdjfh"
+    p params
+    @reservation = Reservation.find_by_id(params[:reservation_id])
+    @rater_id = @reservation.occupant_id
+    @user_id = @reservation.space.location.user_id
+    @rating = Rating.create({score: params[:score], reservation_id: params[:reservation_id], rater_id: @rater_id, user_id: @user_id})
+    redirect_to space_path(@reservation.space_id)
+  end
+end
