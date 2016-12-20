9ceda6c1ed9346750b35c6544a41c7cb671466bc
conorburke <cjburke89@gmail.com>
2016-12-19 21:15:10 -0800

    add redis gem, update app.js, cable.yml and routes for cable path

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :chat_rooms, only: [:new, :create, :show, :index]
+  mount ActionCable.server => '/cable'
+
   resources :spaces do
     resources 'reservations', only: [:create]
   end
