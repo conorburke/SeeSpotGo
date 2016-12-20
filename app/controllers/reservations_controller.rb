6c520718603e1a24cb5aee494468a39662a24195
Patrick Anderson <email": null>
2016-12-20 09:46:34 -0800

    refactor reservations controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,11 +1,10 @@
 class ReservationsController < ApplicationController
   def create
-
     @reservation = Reservation.create(reservation_params)
     redirect_to space_path(@reservation.space_id)
   end
 
   def reservation_params
-    params.require(:reservation).permit(:start_time, :end_time, :space_id, :occupant_id)
+    params.require(:reservation).permit(:start_time, :end_time).merge(space_id: params[:space_id], occupant_id: current_user.id)
   end
 end
