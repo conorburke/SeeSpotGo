3137095b97598c7aecefc74614e2f11146d0d60d
conorburke <cjburke89@gmail.com>
2016-12-21 10:44:35 -0800

    add gravatar to chat rooms

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def gravatar_for(user, opts = {})
+    opts[:alt] = user.email.split('@')[0]
+    image_tag "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}?r=pg&s=#{opts.delete(:size) { 40 }}",
+              opts
+  end
 end
